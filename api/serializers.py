b33e90ffd3c534c9e496acc3389354bac62520b1
Marat17 <muhutdinov@hotmail.com>
2018-11-26 03:14:00 +0300

    user field added to api

--- api/serializers.py
+++ api/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class CatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cat
-        fields = ('name','age', 'breed', 'hair_color')
+        fields = ('name','age', 'breed', 'hair_color', 'user')
