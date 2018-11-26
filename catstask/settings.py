d85d8abd052c7c24f56df764ef6136a49827f7f9
Marat17 <muhutdinov@hotmail.com>
2018-11-26 11:32:12 +0300

    db

--- catstask/settings.py
+++ catstask/settings.py
@@ -87,15 +87,13 @@ WSGI_APPLICATION = 'catstask.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 # Register database schemes in URLs.
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
+    'default' : {
+    'ENGINE': 'django.db.backends.mysql',
+    'HOST': 'us-cdbr-iron-east-01.cleardb.net',
+    'USER': 'b516d240faa43e',
+    'NAME': 'heroku_61b9131ca3b0102',
+    'PASSWORD': os.environ.get('DBP'),
+}
 }
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
